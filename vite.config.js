5de0ab88260a830d04060f8594ccdb688551ee33
lorenvitale <133125638+lorenvitale@users.noreply.github.com>
2025-10-08 10:01:52 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
 export default defineConfig({
   plugins: [react()],
-  base: "/provvcalc/", // necessario per GitHub Pages (sotto-cartella)
+  base: "/PERCENTCALC/",   // <-- deve essere il NOME ESATTO del repo
 });
